b03e12ac5a633fce72f5addac0f7a4e69050d8d4
k15001kk <k15001kk@pck15001kk.local>
2017-07-06 15:58:15 +0900

    first commit

---
+++ practice/practice-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  practice-Bridging-Header.h
+//  practice
+//
+//  Created by k15001kk on 2017/06/28.
+//  Copyright © 2017年 AIT. All rights reserved.
+//
+
+#ifndef practice_Bridging_Header_h
+#define practice_Bridging_Header_h
+
+#import <NCMB/NCMB.h>
+
+#endif /* practice_Bridging_Header_h */
